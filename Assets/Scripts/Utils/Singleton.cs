0a20be588c2b4b03522d166d24e18ee33b6a0e8f
Donghee <39627402+luz0415@users.noreply.github.com>
2025-06-23 02:09:03 +0900

    Add | 싱글톤 제네릭 추가
    - Singleton, Scenegleton(한 씬에서만 사용하는 싱글톤) 추가

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// MonoBehaviour를 상속하는 싱글톤.
+/// 씬이 바뀌어도 사라지지 않는다.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    GameObject container = new GameObject(nameof(T));
+                    _instance = container.AddComponent<T>();
+                    DontDestroyOnLoad(_instance);
+                }
+            }
+            return _instance;
+        }
+    }
+
+    public virtual void Awake()
+    {
+        RemoveDuplicates();
+    }
+    
+    private void RemoveDuplicates()
+    {
+        if(_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
