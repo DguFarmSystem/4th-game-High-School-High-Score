bdbdca7d1bdfe4d869bd59aeba0d2badd9d3dd49
Junil-Kwon <kjil0423@gmail.com>
2025-07-02 03:00:40 +0900

    Add | 창문닫기 스테이지 추가 및 관련 스크립트 초기화

---
+++ Assets/Scripts/Stage/Stages/WindowClosingStage.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using Stage;
+using UnityEngine;
+
+public class WindowClosingStage : StageNormal
+{
+    // Lifecycle methods
+    public void Awake()
+    {
+        OnStageStart();
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
