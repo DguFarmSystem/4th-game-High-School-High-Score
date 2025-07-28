5780a15da1485017455330638ac4c7ff8f3dca00
Junil-Kwon <kjil0423@gmail.com>
2025-07-28 18:48:41 +0900

    Add | 마이쮸던지기 스테이지 씬 초기 설정

---
+++ Assets/Scripts/Stage/Stages/SnackThrowingStage/SnackThrowingStage.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Stage;
+using System.Linq;
+using UnityEditor.Rendering;
+
+public class SnackThrowingStage : StageNormal
+{
+    [SerializeField] public int stageLevel;
+
+    // public StageState CurrentState => CurrentStageState;
+
+    // TEST CODE
+    [SerializeField] private GameObject _greenSphere;
+    [SerializeField] private GameObject _redSphere;
+
+    public override void OnStageStart()
+    {
+        base.OnStageStart();
+    }
+    protected override void OnStageEnd()
+    {
+        base.OnStageEnd();
+    }
+
+    protected override void OnStageClear()
+    {
+        base.OnStageClear();
+    }
+
+    private void OnStageEndedGimmik(bool isStageCleared)
+    {
+
+        if (isStageCleared)
+        {
+            //TEST CODE
+            Debug.Log("Stage cleared!");
+            _greenSphere.SetActive(true);
+        }
+        else
+        {
+            //TEST CODE
+            Debug.Log("Stage failed!");
+            _redSphere.SetActive(true);
+        }
+    }
+
+    // ============ Lifecycle methods ============ //
+    public void OnEnable()
+    {
+        OnStageEnded += OnStageEndedGimmik;
+    }
+
+    public void OnDisable()
+    {
+        OnStageEnded -= OnStageEndedGimmik;
+    }
+
+    void Start()
+    {
+
+        // 스테이지 시작
+        OnStageStart();
+    }
+
+
+    void Update()
+    {
+        /*
+        if (/*스테이지 클리어 조건 만족 &&
+        CurrentStageState == StageState.Playing)
+        {
+            OnStageClear(); // 모든 조건이 완료되면 스테이지 클리어 처리
+        }
+        */
+    }
+}
