c28ebee3225a32d76e86270ced4c0d93b436c940
coffeepott <ksh1643890@gmail.com>
2025-12-17 22:22:01 +0900

    Refactor. 스테이지 음악 추가 및 폴리싱
    - 불필요한 파일을 지우고, 코드 가다듬기

--- Assets/Scripts/Stage/Stages/BoardStage/BlackBoardStage.cs
+++ Assets/Scripts/Stage/Stages/BoardStage/BlackBoardStage.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Stage;
 
 /*
@@ -11,11 +12,22 @@ using Stage;
  */
 public class BlackBoardStage : StageNormal
 {
+    [Header("Audio Setting")]
+    [SerializeField] private AudioSource EffectAudio;
+    [SerializeField] private AudioClip[] EffectSound;
+
+    [Header("Stage Resource")]
+    //난이도별 스테이지에서 필요한 리소스를 지정하는 코드
+    public Sprite[] backgrounds;                   //배경 이미지 설정
+    public Sprite[] Erasers;                       //지우개 이미지 설정
+    [SerializeField] private Image image;          //선택한 배경 이미지를 넣을 이미지 오브젝트
+    [SerializeField] private GameObject Eraser;    //지우개 이미지 지정
+
     [Header("Prefab & Stage Config")]
     public EraseTexture[] erasePrefabs;   // 프리팹 (지울 이미지)
     public Transform spawnParent;      // 스폰할 부모 오브젝트 (예: Canvas나 특정 위치)
     public float clearThreshold = 0.8f; // 클리어 목표 (0~1 사이 값)
-    private static int stageLevel = 1;  // 나중에 StageManager에서 stageLevel을 넘겨주는 방식으로 리소스 지정
+    private static int stageLevel = 0;  // 나중에 StageManager에서 stageLevel을 넘겨주는 방식으로 리소스 지정
 
     private EraseTexture eraseObject;  // 스폰된 EraseTexture 인스턴스
     private float timer;
@@ -65,6 +77,16 @@ public class BlackBoardStage : StageNormal
         //if (stageEnded) return;
         //spawnPoint 월드좌표 기준(0,0)에 배치 
         stageLevel = StageManager.Instance.GetDifficulty() - 1;
+        //stageLevel = 2;
+        //배경, 지우개 오브젝트 설정
+        image.sprite = backgrounds[stageLevel];
+        SpriteRenderer EraseRenderer = Eraser.GetComponent<SpriteRenderer>();
+        EraseRenderer.sprite = Erasers[stageLevel];
+
+        //소리 설정
+        EffectAudio.clip = EffectSound[stageLevel];
+
+        //지울 오브젝트 생성
         eraseObject = Instantiate(erasePrefabs[stageLevel], spawnParent);
         eraseObject.transform.localPosition = Vector3.zero;
         OnStageStart();
@@ -72,10 +94,33 @@ public class BlackBoardStage : StageNormal
 
     void Update()
     {
+        //터치가 입력되고 있는 순간에만 위치 변화
+        if(InputManager.Instance.IsPressing)
+        {
+            Debug.Log("Board Erasing");
+            //매 틱 지우개의 위치 변화
+            Eraser.transform.position = InputManager.Instance.TouchWorldPos - new Vector3(1.0f, 0.0f, 0.0f);
+            
+            // 소리가 재생 중이 아닐 때만 Play
+            if (!EffectAudio.isPlaying)
+            {
+                EffectAudio.Play();
+            }
+        }
+        else
+        {
+            // 터치를 떼었을 때만 Stop
+            if (EffectAudio.isPlaying)
+            {
+                EffectAudio.Stop();
+            }
+
+        }
+
         float erasedRatio = eraseObject.ErasedRatio;
 
         // 디버그 출력
-        Debug.Log($"Erased Ratio: {erasedRatio * 100f:0.00}%");
+        //Debug.Log($"Erased Ratio: {erasedRatio * 100f:0.00}%");
 
         if (erasedRatio >= clearThreshold)
         {
