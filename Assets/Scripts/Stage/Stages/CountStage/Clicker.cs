507234c02b8f8cb932950906af39b088ffc59641
amophi <nawwhod@gmail.com>
2026-03-12 16:53:12 +0900

    Fix: 폰트 에셋 삭제
    Add: 숫자를 세라 스테이지
    
    Add: 숫자를 세라 스테이지 타이머 추가

---
+++ Assets/Scripts/Stage/Stages/CountStage/Clicker.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Clicker : MonoBehaviour
+{
+    public Text countText;
+    public Button myButton;
+    public int countNum = 0;
+
+    void Start()
+    {
+        myButton.onClick.AddListener(CountUp);
+    }
+
+    public void CountUp()
+    {
+        countNum++;
+        countText.text = countNum.ToString();
+    }
+}
