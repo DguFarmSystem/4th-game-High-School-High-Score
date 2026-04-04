fb48a44da24fb18db9bd41cedc84533ea42f98ef
dudare <dudare@naver.com>
2026-04-04 16:06:22 +0900

    저울 스테이지 사운드 추가
    저울 스테이지 사운드 추가

---
+++ Assets/Scripts/Stage/Stages/ScalesStage/Weight.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weight : MonoBehaviour
+{
+
+    public AudioClip Effect;
+    AudioSource theaudio;
+
+    public bool IsInRange=false;
+
+    void Start()
+    {
+        theaudio.GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.gameObject.transform.position.y<transform.position.y)
+        {
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = GetComponent<SpriteRenderer>().sortingOrder - 1 ;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.transform.position.y < transform.position.y && IsInRange)
+        {
+            theaudio.PlayOneShot(Effect);
+        }
+    }
+
+}
