51ae6c95cf6c9697475df16e3f895c24f3f26893
amophi <nawwhod@gmail.com>
2026-04-10 22:44:18 +0900

    feat: 숫자를 세라 스테이지 사운드 추가

--- Assets/Scripts/Stage/Stages/CountStage/Clicker.cs
+++ Assets/Scripts/Stage/Stages/CountStage/Clicker.cs
@@ -6,16 +6,24 @@ public class Clicker : MonoBehaviour
 {
     public Text countText;
     public Button myButton;
+    public AudioSource audioSource;
+    public AudioClip audioClip;
     public int countNum = 0;
 
     void Start()
     {
         myButton.onClick.AddListener(CountUp);
+        myButton.onClick.AddListener(ClickSound);
     }
 
     public void CountUp()
     {
         countNum++;
         countText.text = countNum.ToString("D5");
+    }
+
+    public void ClickSound()
+    {
+        audioSource.PlayOneShot(audioClip);
     }
 }
